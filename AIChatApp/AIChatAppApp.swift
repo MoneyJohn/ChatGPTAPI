b57da9c3169cb258dd692f38dd82c6bc33adc177
John <money.john2006@gmail.com>
2023-03-29 12:20:15 -0700

    Initial Commit

---
+++ AIChatApp/AIChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatAppApp.swift
+//  AIChatApp
+//
+//  Created by John Money on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct AIChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
