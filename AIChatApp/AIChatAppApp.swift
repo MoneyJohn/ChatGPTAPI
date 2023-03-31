3c348f470110acd11b6fd3ec6b084bd33c861396
John <money.john2006@gmail.com>
2023-03-31 09:12:40 -0700

    first

--- AIChatApp/AIChatAppApp.swift
+++ AIChatApp/AIChatAppApp.swift
@@ -4,9 +4,8 @@
 //
 //  Created by John Money on 3/29/23.
 //
-
+import OpenAISwift
 import SwiftUI
-
 @main
 struct AIChatAppApp: App {
     var body: some Scene {
--- AIChatApp/ContentView.swift
+++ AIChatApp/ContentView.swift
@@ -4,18 +4,69 @@
 //
 //  Created by John Money on 3/29/23.
 //
-
+import OpenAISwift
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+final class ViewModel: ObservableObject {
+    init() {}
+    
+    private var client: OpenAISwift?
+    
+    func setup() {
+        client = OpenAISwift(authToken: "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")
+    }
+    func send(text: String,
+              completion: @escaping (String) -> Void) {
+        client?.sendCompletion(with: text, maxTokens: 500, completionHandler: { result in switch result {
+        case .success(let model):
+            let output = model.choices.first?.text ?? ""
+            completion(output)
+        case .failure:
+            break
         }
-        .padding()
+            
+        })
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var models = [String]()
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                ForEach(models, id: \.self) {string in
+                    Text(string)
+                }
+                Spacer()
+                
+                HStack {
+                    TextField("Chat here...", text: $text)
+                    Button("Ask!") {
+                        ask()
+                    }
+                    
+                }
+            }
+            .onAppear {
+                viewModel.setup()
+            }
+            .padding()
+        }
+    }
+    func ask() {
+        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        models.append("Me: \(text)")
+        viewModel.send(text: text) {response in
+            DispatchQueue.main.async {
+                self.models.append("MONEY: "+response)
+                self.text = ""
+            }
+        }
     }
 }
 
